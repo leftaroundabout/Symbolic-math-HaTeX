583c1b8725d0b0db3123c359ca4f41eb70f6034d
Justus Sagemüller <sagemuej@smail.uni-koeln.de>
2012-12-22 16:04:30 +0100

    Added a (very) few comments

--- HaTeX_SymMath_test0.hs
+++ HaTeX_SymMath_test0.hs
@@ -16,12 +16,77 @@ import Data.Function
 import Data.Ratio
 import Data.Functor.Contravariant
 
+
+
+-- Document structure copied from Daniel Diaz' 'Examples/simple.hs'.
+
+
+main :: IO ()
+main = execLaTeXT simple >>= renderFile "shorttest0.tex"
+
+simple :: Monad m => LaTeXT_ m
+simple = do
+   thePreamble
+   document theBody
+
+mathTestFloating :: (Monad m, Floating n, Show n) => LaTeXT m n
+mathTestFloating = wDefaultTeXMathDisplayConf $ do
+   lift "For "
+   x <- mathDefinition "x" 19
+   lift " and "
+   tau <- mathDefinition "\\tau" $ 2*pi
+   lift ", "
+   displayMathExpr $
+              2 + 7*(6 - tau) - exp(5 - sqrt(x**2 + 4/pi))
+              
+mathTestInteger :: Monad m => LaTeXT m Integer
+mathTestInteger = wDefaultTeXMathDisplayConf $ do
+   displayMathExpr $
+             4 ^: (2 ^: 3) ^: 2 - 10000^7
+
+thePreamble :: Monad m => LaTeXT_ m
+thePreamble = do
+   documentclass [] article
+   usepackage [utf8] inputenc
+   author "Justus Sagemüller"
+   title "Simple example"
+
+theBody :: Monad m => LaTeXT_ m
+theBody = do
+   maketitle
+   section "Hello"
+   "This is a simple example using the "
+   hatex
+   " library and some math stuff. "
+   
+   n <- mathTestInteger
+   " is "
+   rendertex n
+   ". "
+   
+   newline
+   x <- mathTestFloating
+   " is approximately "
+   wDefaultTeXMathDisplayConf . inlineMathExpr $ prettyFloatApproxExprn x
+   ". "
+
+
+
+
+
+
+
+
+
+
+
+-- Some trivial fixed-size-array functors, these model the structural recursion of
+-- either primitive math (None), unary functions (Identity), or infix functions (Pair).
 data None a = None
 instance Functor None where { fmap _ None = None }
 data Pair a = Pair a a
 instance Functor Pair where { fmap f (Pair l r) = Pair (f l) (f r) }
 
-type TeXMathDisplayConf = ()
 
 
 
@@ -35,9 +100,9 @@ isotropFixity (Infixl n) = n
 isotropFixity (Infixr n) = n
 
 data MathEvaluation res arg where
---   MathPrimitive { mathPrimVal :: res
---                 , mathPrimId :: MathPrimtvId
---                 } :: MathEvaluation arg res
+--   MathPrimitive { mathPrimVal :: res              -- MathEnvd is actually a generalisation 
+--                 , mathPrimId :: MathPrimtvId      -- of all possible contructors; it's not
+--                 } :: MathEvaluation arg res       -- particularly efficient though.
 --   MathDepdPrimitive { mathDpPrimtveVal :: arg -> res
 --                     , mathDpPrimtveId :: MathPrimtvId -> MathPrimtvId
 --                     } :: MathEvaluation arg res
@@ -251,6 +316,12 @@ data TeXMathExpr = TeXMathExpr { mathExprRendered :: Text
                                , mathExpr_isSimple :: Bool
                                , mathExpr_fixity :: Int 
                                }
+                               
+                               
+          -- TeXMathDisplayConf should eventually contains things
+          -- like the default way to render e.g. multiplication
+          -- ('\cdot' vs '\times' or what environments to use.
+type TeXMathDisplayConf = ()
 
 
 type MathematicalLaTeXT m a = ReaderT TeXMathDisplayConf (LaTeXT m) a
@@ -272,55 +343,3 @@ wDefaultTeXMathDisplayConf = (`runReaderT`())
 --   TeXMathExpr 
 
 
-
--- By executing 'execLaTeXT' you run the 'LaTeXT' monad and make a 'LaTeX' value as output.
--- With 'renderFile' you render it to 'Text' and write it in a file.
-main :: IO ()
-main = execLaTeXT simple >>= renderFile "shorttest0.tex"
-
--- It's a good idea to separate the preamble of the body.
-simple :: Monad m => LaTeXT_ m
-simple = do
-   thePreamble
-   document theBody
-
-mathTestFloating :: (Monad m, Floating n, Show n) => LaTeXT m n
-mathTestFloating = wDefaultTeXMathDisplayConf $ do
-   lift "For "
-   x <- mathDefinition "x" 19
-   lift " and "
-   tau <- mathDefinition "\\tau" $ 2*pi
-   lift ", "
-   displayMathExpr $
-              2 + 7*(6 - tau) - exp(5 - sqrt(x**2 + 4/pi))
-              
-mathTestInteger :: Monad m => LaTeXT m Integer
-mathTestInteger = wDefaultTeXMathDisplayConf $ do
-   displayMathExpr $
-             4 ^: (2 ^: 3) ^: 2 - 10000^7
-
--- Preamble with some basic info.
-thePreamble :: Monad m => LaTeXT_ m
-thePreamble = do
-   documentclass [] article
-   usepackage [utf8] inputenc
-   author "Justus Sagemüller"
-   title "Simple example"
-
--- Body with a section.
-theBody :: Monad m => LaTeXT_ m
-theBody = do
-   maketitle
-   section "Hello"
-   "This is a simple example using the "
-   hatex
-   " library and some math stuff. "
-   n <- mathTestInteger
-   " is "
-   rendertex n
-   ". "
-   newline
-   x <- mathTestFloating
-   " is approximately "
-   wDefaultTeXMathDisplayConf . inlineMathExpr $ prettyFloatApproxExprn x
-   ". "
