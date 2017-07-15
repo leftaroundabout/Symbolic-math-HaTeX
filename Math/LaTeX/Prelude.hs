90f8d23de5766d4c0e4c58146415d607aa73e61c
Justus Sagemüller <sagemueller@geo.uni-koeln.de>
2017-07-15 23:59:34 +0200

    Assemble something of a proper prelude module.

--- Math/LaTeX/Prelude.hs
+++ Math/LaTeX/Prelude.hs
@@ -7,15 +7,41 @@
 -- Stability   : experimental
 -- Portability : requires GHC>7 extensions
 -- 
+{-# LANGUAGE OverloadedStrings #-}
 
+module Math.LaTeX.Prelude (
+   -- * Use in documents
+     (>$), dmaths
+   -- * Primitive symbols
+   , module CAS.Dumb.Symbols.Unicode.MathLatin_RomanGreek__BopomofoGaps
+   -- ** Modifiers
+   , (%$>), prime, LaTeX.bar, LaTeX.hat, LaTeX.vec, LaTeX.underline
+   -- * Operators
+   , (°), (⁀), (...), (،..،), (،), (␣), (+..+), (*..*), (×), (⊗), (∘), factorial
+   , (◝), (◝⁀), (◞), (◞◝), (|◞), (|◝), (|◞◝)
+   , (⩵), (≡), (⸪=), (=⸪), (⩵!), (≠), (⪡), (⪢), (≤), (≥), (≪), (≫), (₌₌)
+   , (=→), (≈), (∼)
+   , (⊂), (/⊂), (⊆), (⊃), (⊇), (∋), (∌), (∈), (∉), (∩), (∪), (-\-), (⸪), (⊕)
+   , (∀:), (∃:)
+   , (-→), (↦), (↪), (==>), (<==), (<=>), (∧), (∨)
+   , (∫), (◞∫), (◞∮), d, (∑), (◞∑), (∏), (◞∏), del, nabla
+   , infty
+   ) where
 
-module Math.LaTeX.Prelude where
-
+import CAS.Dumb.Symbols.Unicode.MathLatin_RomanGreek__BopomofoGaps hiding ((%$>))
+import CAS.Dumb.Symbols
 import Math.LaTeX.Internal.MathExpr
+import Math.LaTeX.Internal.Display
+
+import Text.LaTeX.Base.Class (LaTeXC)
+import Text.LaTeX.Base (raw)
+import qualified Text.LaTeX.Packages.AMSMath as LaTeX
+import qualified Text.LaTeX.Base.Commands as LaTeX
+
+import Data.Monoid
 
 
 
-
-
-
+prime :: LaTeXC l => l -> l
+prime = (<>raw"'")
 
